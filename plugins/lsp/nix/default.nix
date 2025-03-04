0aaa3d9a55fdf7d190703d5218e5234162adf8f5
Lucas Slebos <lucas@slebos.nz>
2025-03-04 23:07:42 +1300

    add nixd nixos and home-manager expressions

--- plugins/lsp/nix/default.nix
+++ plugins/lsp/nix/default.nix
@@ -1,3 +1,19 @@
 {
-  plugins.lsp.servers.nixd.enable = true;
+  plugins.lsp.servers.nixd = {
+    enable = true;
+    settings = {
+      nixpkgs = {
+        expr = /* nix */ "import <nixpkgs> { }";
+      };
+      formatting.command = ["nixfmt"];
+      options = rec {
+        nixos = {
+          expr = /* nix */ "(builtins.getFlake (\"git+file://\" + toString ./.)).nixosConfigurations.artemis.options";
+        };
+        home_manager = {
+          expr = nixos.expr + /* nix */ ".home-manager.users.type.getSubOptions []";
+        };
+      };
+    };
+  };
 }

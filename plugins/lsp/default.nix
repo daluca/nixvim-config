d6489752e1512a464efc7b31799b770817d46561
Lucas Slebos <lucas@slebos.nz>
2025-06-20 11:13:34 +0200

    add typos_lsp server

--- plugins/lsp/default.nix
+++ plugins/lsp/default.nix
@@ -6,6 +6,7 @@
     ./lua
     ./nix
     ./rust
+    ./typos
     ./yaml
   ];
 
---
+++ plugins/lsp/typos/default.nix
@@ -0,0 +1,8 @@
+{
+  plugins.lsp.servers.typos_lsp = {
+    enable = true;
+    extraOptions = {
+      init_options.diagnosticSeverity = "Hint";
+    };
+  };
+}

48c7756c5b357d13df171e3ca3c38abb19b0913e
Lucas Slebos <lucas@slebos.nz>
2025-07-11 15:04:07 +0200

    add relative file path to lualine

--- plugins/lualine/default.nix
+++ plugins/lualine/default.nix
@@ -1,3 +1,14 @@
 {
-  plugins.lualine.enable = true;
+  plugins.lualine = {
+    enable = true;
+    settings = {
+      sections.lualine_c = [
+        {
+          __unkeyed-1 = "filename";
+          file_status = true;
+          path = 1;
+        }
+      ];
+    };
+  };
 }

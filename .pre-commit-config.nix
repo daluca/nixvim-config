5d8e6cae591b26cbc77b3feafa76fc4b3ef4ea21
Lucas Slebos <lucas@slebos.nz>
2025-08-22 12:54:47 +0200

    add additional pre-commit hooks

--- .pre-commit-config.nix
+++ .pre-commit-config.nix
@@ -1,4 +1,14 @@
 {
+  check-added-large-files.enable = true;
+  check-merge-conflicts.enable = true;
+  detect-private-keys.enable = true;
+  end-of-file-fixer.enable = true;
+  forbid-new-submodules.enable = true;
+  trim-trailing-whitespace.enable = true;
+  deadnix = {
+    enable = true;
+    settings.edit = true;
+  };
   typos = {
     enable = true;
     settings.configPath = "typos.toml";
